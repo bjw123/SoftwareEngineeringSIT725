2bb721665b5382e92f35c1a08060cfa2ac278620
bjw123 <brycewilkinson43@gmail.com>
2020-07-16 14:45:03 +0900

    biosensor attempt
    initial attempt at biosensor
    
    *not working (test upload)

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,17 @@
 console.log("hello world");
 console.log("goodbye cruel world");
+
+class biosensor { //heartbeat simulation class
+    constructor(name) {
+        this.person = name;
+        this.heartRate = 0;
+        var min = 60;
+        var max = 80
+    }
+    heartBeat() { //generate heartbeat
+        setInterval(this.heartBeat(), 1000);
+    }
+    generateHeartBeat() {
+        console.log(parseInt(Math.random() * (max - min) + min));
+    }
+}
